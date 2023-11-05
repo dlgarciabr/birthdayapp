eb017af149360baef54bcfaae5a31a38cdb2a251
Daniel <dlgarciabr@gmail.com>
2023-11-05 13:44:25 +0000

    code improvements

--- src/core/components/LabeledSelect.tsx
+++ src/core/components/LabeledSelect.tsx
@@ -1,58 +1,48 @@
-import { forwardRef, PropsWithoutRef } from 'react';
-import { useField, useFormikContext, ErrorMessage } from 'formik';
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { forwardRef, PropsWithoutRef } from "react"
+import { useField, useFormikContext, ErrorMessage } from "formik"
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material"
 
-export interface LabeledSelectProps extends PropsWithoutRef<JSX.IntrinsicElements['select']> {
-  name: string;
-  label: string;
-  outerProps?: PropsWithoutRef<JSX.IntrinsicElements['div']>;
-  fullWidth?: boolean;
-  items: { value: string | number; label: string }[];
+export interface LabeledSelectProps extends PropsWithoutRef<JSX.IntrinsicElements["select"]> {
+  name: string
+  label: string
+  outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
+  fullWidth?: boolean
+  items: { value: string | number; label: string }[]
 }
 
 export const LabeledSelect = forwardRef<HTMLSelectElement, LabeledSelectProps>(
   ({ name, label, items, outerProps, placeholder, ...props }, ref) => {
-    const [input, meta, helper] = useField(name);
-    const { isSubmitting } = useFormikContext();
-    const labelId = `select-label-${name}`;
+    const [input, meta, helper] = useField(name)
+    const { isSubmitting } = useFormikContext()
+    const labelId = `select-label-${name}`
     return (
       <div {...outerProps}>
         <FormControl fullWidth error={!!meta.error}>
-          <InputLabel id={labelId} >
-            {label}
-          </InputLabel>
+          <InputLabel id={labelId}>{label}</InputLabel>
           <Select
-            {...input} 
+            {...input}
             labelId={labelId}
             name={name}
-            onChange={              
-              async (event) => {
-                await helper.setValue(event.target.value.toString());
-              }
-            }
+            onChange={async (event) => {
+              await helper.setValue(event.target.value.toString())
+            }}
             disabled={isSubmitting}
-            variant='outlined'
-            color='primary'
-            size='medium'
+            variant="outlined"
+            color="primary"
+            size="medium"
             ref={ref}
             fullWidth={props.fullWidth}
             id="demo-simple-select"
           >
-            {
-              items.map((item) => (
-                <MenuItem 
-                  key={Math.random().toString(36).substring(2, 15)} 
-                  value={item.value}>
-                    {item.label}
-                </MenuItem>
-              ))
-            }
+            {items.map((item) => (
+              <MenuItem key={Math.random().toString(36).substring(2, 15)} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
-        <div role='alert' className='form-error-message'>
-          <ErrorMessage name={name}>
-            {(msg) => (msg)}
-          </ErrorMessage>
+        <div role="alert" className="form-error-message">
+          <ErrorMessage name={name}>{(msg) => msg}</ErrorMessage>
         </div>
 
         <style jsx>{`
@@ -71,8 +61,8 @@ export const LabeledSelect = forwardRef<HTMLSelectElement, LabeledSelectProps>(
           }
         `}</style>
       </div>
-    );
+    )
   }
-);
+)
 
-export default LabeledSelect;
+export default LabeledSelect
